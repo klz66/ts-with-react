156daf36870866f612b4131c1d7bf3819c8a7e68
29717 <kailong.zhong@hand-china.com>
2021-01-31 00:02:31 +0800

    1/30

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,7 @@
 /*
  * @Description: 
  * @Author: Zhong Kailong
- * @LastEditTime: 2021-01-30 23:38:53
+ * @LastEditTime: 2021-01-31 00:02:11
  */
 
 import 'antd/dist/antd.css'
@@ -10,24 +10,38 @@ import { connect } from 'react-redux'
 import { getTodoList, getInputChangeAction, getAddItemAction, getDeleteItemAction} from './store/actionCreators';
 
 function TodoList(props) {
+  const { inputValue, list, changeInputValue,handClick,handDelete} = props
   console.log(props,2020);
   // const [state, setState] = useState(store.getState())
   return (
     <div style={{marginTop:'20px'}}>
-      <input value={props.state.inputValue} onChange={(e)=>{props.changeInputValue(e)}}/>
-      <button>{props.state.inputValue}</button>
+      <input value={inputValue} onChange={(e)=>{changeInputValue(e)}}/>
+      <button onClick={()=>handClick(inputValue)}>提交</button>
+      {
+        list.map((item,index) => {
+          return <div onClick={()=>handDelete(index)}>{ item }</div>
+        })
+      }
     </div>
   );
 }
 
 const mapStateToProps = (state) => ({
   inputValue: state.inputValue,
-  state:state,
+  list:state.list,
 })
 const mapDispatchToProps = (dispatch) => ({
   changeInputValue(e){
     console.log(e.target.value);
     const action = getInputChangeAction(e.target.value);
+    dispatch(action)
+  },
+  handClick(inputValue){
+    const action = getAddItemAction(inputValue);
+    dispatch(action)
+  },
+  handDelete(index){
+    const action = getDeleteItemAction(index);
     dispatch(action)
   }
 })
